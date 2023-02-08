1ac402e96496d80c101d972669bd50fdd68c12d5
Aerdellosi <johnhanifin.webdev@gmail.com>
2023-02-07 16:40:18 -0800

    attempt to fix remove class hidden

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 function getDinoName() {
+
   let request = new XMLHttpRequest();
   const url = `https://dinoipsum.com/api/?format=json&words=1&paragraphs=1`;
 
   request.addEventListener("loadend", function () {
     const response = JSON.parse(this.responseText);
+    console.log(response + "H E R E H E R E");
   
     if (this.status === 200) {
-      document.getElementById("hidden1").style.display = "block";
-     
+      document.getElementById("hidden1").classList.remove("hidden");
+
       return response;
       // printElements(response);
     } else {
@@ -27,6 +29,6 @@ function getDinoName() {
 
 
 window.addEventListener("load", function () {
-  document.getElementById("form1").addEventListener("submit", getDinoName);
+  document.getElementById("form1").addEventListener("click", getDinoName);
 
 });
